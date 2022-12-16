ba7a4b5eec6a227de52bdc114ac82014d11eb2c1
kazuyainoue <bauer24tbp@gmail.com>
2022-12-17 00:56:15 +0900

    PostContent.jsxとPostContent.css、ルーティングを追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
 import CreatePost from "./components/CreatePost";
+import PostContent from "./components/PostContent";
 import EditPost from "./components/EditPost";
 import UserProfile from "./components/UserProfile";
 import EditUser from "./components/EditUser";
@@ -21,6 +22,7 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/logout" element={<Logout />}></Route>
         <Route path="/posts/new" element={<CreatePost />}></Route>
+        <Route path="/posts/:postId" element={<PostContent />}></Route>
         <Route path="/posts/:postId/edit" element={<EditPost />}></Route>
         <Route path="/users/:userId" element={<UserProfile />}></Route>
         <Route path="/users/:userId/edit" element={<EditUser />}></Route>
