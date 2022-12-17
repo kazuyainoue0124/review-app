6bcaa2cf4657df74027b3417863cfafe4644bb43
kazuyainoue <bauer24tbp@gmail.com>
2022-12-17 20:33:52 +0900

    ホーム・新規登録・ログイン画面のレイアウトと画面遷移を実装

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import * as React from "react";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
 
-const Footer = () => {
-  return <div>Footer</div>;
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+function Footer(props) {
+  const { description, title } = props;
+
+  return (
+    <Box component="footer" sx={{ bgcolor: "background.paper", py: 3, mt: 4, borderTop: 1, borderColor: "divider" }}>
+      <Container maxWidth="lg">
+        <Typography variant="h6" align="center" gutterBottom>
+          {title}
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          {description}
+        </Typography>
+        <Copyright />
+      </Container>
+    </Box>
+  );
+}
+
+Footer.propTypes = {
+  description: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default Footer;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+import LoginIcon from '@mui/icons-material/Login';
+import PersonIcon from '@mui/icons-material/Person';
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = (props) => {
+  const { title } = props;
+  return (
+    <React.Fragment>
+      <Toolbar sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Typography
+          component={Link}
+          to="/"
+          variant="h6"
+          color="inherit"
+          align="left"
+          noWrap
+          sx={{ flex: 1 }}
+          style={{ textDecoration: 'none' }}
+        >
+          {title}
+        </Typography>
+        {/* <IconButton>
+          <SearchIcon />
+        </IconButton> */}
+        <Button variant="outlined" size="middle" component={Link} to="/login">
+          <LoginIcon sx={{ mr: 1 }} />
+          ログイン
+        </Button>
+        <Button
+          variant="outlined"
+          size="middle"
+          component={Link}
+          sx={{ ml: 1 }}
+          to="/signup"
+        >
+          <PersonIcon sx={{ mr: 1 }} />
+          新規登録
+        </Button>
+      </Toolbar>
+    </React.Fragment>
+  );
 };
 
 export default Header;
